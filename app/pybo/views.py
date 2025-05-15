cc1ea3e412f1dd71124e85e2e94e812f92dbe7ad
ParkJaeHyun <mrone1126@gmail.com>
2025-05-15 15:48:05 +0900

    feat: list 완성함

--- app/pybo/views.py
+++ app/pybo/views.py
@@ -4,3 +4,10 @@ from .models import Problem
 def index(request):
     question_list = Problem.objects.all().order_by('index')
     return render(request, 'index.html', {'question_list': question_list})
+
+def ex_list(request):
+    question_list = Problem.objects.all().order_by('index')
+    return render(request, 'ex_list.html', {'question_list': question_list})
+
+def header(request):
+    return render(request, 'header.html')
